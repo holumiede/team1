8b90b10fe9cbac814ee19e6f057bf6c74e74576d
Osideinde Oluwadara <osideindeo@gmail.com>
2017-08-19 19:24:19 +0100

    Add files via upload

---
+++ participants.php
@@ -0,0 +1,115 @@
+<?php include "header.php" ?>
+
+<div id="demo">
+    <div id="demo">
+        <h1 style="color:white;">Participants</h1>
+
+
+        <!-- Responsive table starts here -->
+        <!-- For correct display on small screens you must add 'data-title' to each 'td' in your table -->
+        <div class="table-responsive-vertical shadow-z-1">
+
+
+            <!-- Responsive table starts here -->
+            <!-- For correct display on small screens you must add 'data-title' to each 'td' in your table -->
+            <div class="table-responsive-vertical shadow-z-1">
+                <!-- Table starts here -->
+                <table id="table" class="table table-hover table-mc-light-blue">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Link</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td data-title="ID">1</td>
+                            <td data-title="Name">User 1</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" target="_blank">GitHub</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">2</td>
+                            <td data-title="Name">User 2</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" target="_blank">Github</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">3</td>
+                            <td data-title="Name">User 3</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" class="icon fa-github" target="_blank">GitHub</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">4</td>
+                            <td data-title="Name">User 4</td>
+                            <td data-title="Link"><a href="#" class="icon fa-github" target="_blank">Github</a></td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">5</td>
+                            <td data-title="Name">User 5</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" target="_blank">Github</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">6</td>
+                            <td data-title="Name">User 6</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" target="_blank">Github</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">7</td>
+                            <td data-title="Name">User 7</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" target="_blank">Github</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">8</td>
+                            <td data-title="Name">User 8</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" target="_blank">Github</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">9</td>
+                            <td data-title="Name">User 9</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" target="_blank">Github</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                        <tr>
+                            <td data-title="ID">10</td>
+                            <td data-title="Name">User 10</td>
+                            <td data-title="Link">
+                                <a href="#" class="icon fa-github" target="_blank">Github</a>
+                            </td>
+                            <td data-title="Status">Completed</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+
+        </div>
+
+        </body>
+
+        </html>
