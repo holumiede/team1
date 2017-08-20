fe756b1ea9501e59172a437838e69f3919f11113
Osideinde Oluwadara <osideindeo@gmail.com>
2017-08-20 17:07:50 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
+
+
+
 # team3
+
+
+
+
 This is for team3 to review and decide
+
+
+
 Team lead -- Bosunski
+
+
+
 Deputy -- sayopaul2
--- participants.php
+++ participants.php
@@ -2,105 +2,110 @@
     include 'header.php';
 ?>
 <!DOCTYPE html>
-<html>  
-<body>  
-<div id="banner-content" class="row clearfix">
+<html>
 
-    <div class="col-38">
+<body>
+    <div id="banner-content" class="row clearfix">
 
-        <div class="section-heading participants">
-            <h1>Participants Page</h1>
+        <div class="col-38">
+
+            <div class="section-heading participants">
+                <h1>Participants Page</h1>
+            </div>
+
+
+
         </div>
-
-       
 
     </div>
-
-</div><!--End of Row-->
-</header>
+    <!--End of Row-->
+    </header>
 
     <div class="body">
-                    <!-- Table starts here -->
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Link to profile</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>1st user</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Ojo Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr><tr>
-                                <td>3</td>
-                                <td>Ojo Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr><tr>
-                                <td>4</td>
-                                <td>Ojo Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>5</td>
-                                <td>o Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>6</td>
-                                <td>Ojo Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>7</td>
-                                <td>Ojo Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>8</td>
-                                <td>Ojo Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>9</td>
-                                <td>Ojo Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>10</td>
-                                <td>Ojo Oreoluwa</td>
-                                <td>
-                                    <a href="./profile/ogbeniore.html"> ogbeniore's profile</a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-        </div>
+        <!-- Table starts here -->
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Link to profile</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>1st user</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Osideinde Oluwadara</td>
+                    <td>
+                        <a href="./profile/holumiede.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Ojo Oreoluwa</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Ojo Oreoluwa</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>o Oreoluwa</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>Ojo Oreoluwa</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>Ojo Oreoluwa</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>Ojo Oreoluwa</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>9</td>
+                    <td>Ojo Oreoluwa</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>10</td>
+                    <td>Ojo Oreoluwa</td>
+                    <td>
+                        <a href="./profile/ogbeniore.html" class="icon fa-user"> ogbeniore's profile</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </div>
 </body>
+
 </html>
